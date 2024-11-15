889dfefe819f68ca4f83f4d4fbce9f12090d94fb
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-11-15 12:06:59 +0530

    status filter added completed and pending

--- src/components/StatusFilter.jsx
+++ src/components/StatusFilter.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 const StatusFilter = ({ statusFilter, setStatusFilter }) => (
-  <FormControl fullWidth variant="outlined" margin="normal">
-    <InputLabel>Status</InputLabel>
-    <Select
-      value={statusFilter}
-      onChange={(e) => setStatusFilter(e.target.value)}
-      label="Status"
-      sx={{ borderRadius: '8px' }}
-    >
-      <MenuItem value="All">All Status</MenuItem>
-      <MenuItem value="Completed" sx={{ color: 'green' }}>Completed</MenuItem>
-      <MenuItem value="Pending" sx={{ color: 'orange' }}>Pending</MenuItem>
-    </Select>
-  </FormControl>
+  <FormControl variant="outlined" margin="normal" sx={{ minWidth: "50%" }}>
+  <InputLabel sx={{ fontSize: '0.875rem' }}>Status</InputLabel>
+  <Select
+    value={statusFilter}
+    onChange={(e) => setStatusFilter(e.target.value)}
+    label="Status"
+    sx={{
+      fontSize: '0.875rem', 
+      borderRadius: '8px',
+      height: 36, 
+      paddingTop: 0,
+      paddingBottom: 0,
+    }}
+    size="small" 
+  >
+    <MenuItem value="All" sx={{ fontSize: '0.875rem' }}>All Status</MenuItem>
+    <MenuItem value="Completed" sx={{ color: 'green', fontSize: '0.875rem' }}>Completed</MenuItem>
+    <MenuItem value="Pending" sx={{ color: 'orange', fontSize: '0.875rem' }}>Pending</MenuItem>
+  </Select>
+</FormControl>
+
 );
 
 export default StatusFilter;
