ee05821ae0ad0e3aa62e5b8fadd35711482ac07a
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-11-15 12:05:47 +0530

    added priority filter

--- src/components/PriorityFilter.jsx
+++ src/components/PriorityFilter.jsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
-const PriorityFilter = ({ priorityFilter, setPriorityFilter }) => (
-  <FormControl fullWidth variant="outlined" margin="normal">
-    <InputLabel>Priority</InputLabel>
-    <Select
-      value={priorityFilter}
-      onChange={(e) => setPriorityFilter(e.target.value)}
-      label="Priority"
-      sx={{ borderRadius: '8px' }}
-    >
-      <MenuItem value="All">All Priorities</MenuItem>
-      <MenuItem value="High" sx={{ color: 'red' }}>High</MenuItem>
-      <MenuItem value="Medium" sx={{ color: 'orange' }}>Medium</MenuItem>
-      <MenuItem value="Low" sx={{ color: 'green' }}>Low</MenuItem>
-    </Select>
-  </FormControl>
-);
+const PriorityFilter = ({ priorityFilter, setPriorityFilter }) => {
+  useEffect(() => {
+    const savedPriorityFilter = localStorage.getItem('priorityFilter');
+    if (savedPriorityFilter) {
+      setPriorityFilter(savedPriorityFilter);
+    }
+  }, [setPriorityFilter]);
+
+  const handlePriorityChange = (event) => {
+    const selectedPriority = event.target.value;
+    setPriorityFilter(selectedPriority);
+    localStorage.setItem('priorityFilter', selectedPriority);
+  };
+
+  return (
+    <FormControl variant="outlined" margin="normal" sx={{ mr: 1, minWidth: "49%" }}>
+  <InputLabel sx={{ fontSize: '0.875rem' }}>Priority</InputLabel>
+  <Select
+    value={priorityFilter}
+    onChange={handlePriorityChange}
+    label="Priority"
+    sx={{
+      fontSize: '0.875rem',
+      borderRadius: '8px',
+      height: 36,
+      paddingTop: 0,
+      paddingBottom: 0,
+    }}
+    size="small" 
+  >
+    <MenuItem value="All" sx={{ fontSize: '0.875rem' }}>All Priorities</MenuItem>
+    <MenuItem value="High" sx={{ color: 'red', fontSize: '0.875rem' }}>High</MenuItem>
+    <MenuItem value="Medium" sx={{ color: 'orange', fontSize: '0.875rem' }}>Medium</MenuItem>
+    <MenuItem value="Low" sx={{ color: 'green', fontSize: '0.875rem' }}>Low</MenuItem>
+  </Select>
+</FormControl>
+
+  );
+};
 
 export default PriorityFilter;
