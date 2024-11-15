2061bd5fc134818a6b21ba20f9ceddc3f581817c
Yogesh Kumar Sai <yogesh01041961@gmail.com>
2024-11-15 12:06:18 +0530

    search functionality added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,8 +3,13 @@ import { TextField } from '@mui/material';
 
 const SearchBar = ({ searchQuery, setSearchQuery }) => (
   <TextField
+  sx={{
+    flexGrow: 1, 
+    mr: 2
+  }}
     label="Search tasks..."
     variant="outlined"
+    size='small'
     value={searchQuery}
     onChange={(e) => setSearchQuery(e.target.value)}
     fullWidth
